0eb3003f1d0a42d99500190ee460cd80ae7a3e9f
mareberhardt <martineberhardt4@gmail.com>
2020-07-24 15:56:50 -0300

    Including FakerJs and JsonServer to prepare BE

---
+++ server/generate.js
@@ -0,0 +1,18 @@
+var faker = require('faker');
+
+var database = { products: []};
+
+// Faker helps me to automatically generate massive amounts of realistic fake data.
+// In this case I'll generate 200 products (food)
+for (var i = 1; i<= 300; i++) {
+  database.products.push({
+    id: i,
+    name: faker.commerce.productName(),
+    description: faker.commerce.productMaterial(),
+    price: faker.commerce.price(),
+    imageUrl: `https://source.unsplash.com/1600x900/?$product`,
+    quantity: faker.random.number()
+  });
+}
+
+console.log(JSON.stringify(database));
