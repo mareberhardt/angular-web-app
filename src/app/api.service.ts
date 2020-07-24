cc3492708939347947c11a561929fe9985120e68
mareberhardt <martineberhardt4@gmail.com>
2020-07-24 19:00:16 -0300

    Implementing pagination and Error handling

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 
 import {  throwError } from 'rxjs';
-import { retry, catchError } from 'rxjs/operators';
+import { retry, catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  first: string = '';
+  prev: string = '';
+  next: string = '';
+  last: string = '';
 
   private SERVER_URL = 'http://localhost:3000/products';
 
@@ -27,9 +31,50 @@ export class ApiService {
   }
 
   public sendGetRequest(){
-    return this.httpClient.get(this.SERVER_URL)
-      .pipe(
-        catchError(this.handleError)
+    // Add safe, URL encoded _page and _limit parameters
+    return this.httpClient.get(this.SERVER_URL, {
+      params: new HttpParams({fromString: '_page=1&_limit=20'}),
+      observe: 'response'
+    }).pipe(
+      retry(3),
+      catchError(this.handleError),
+      tap(res => {
+        console.log(res.headers.get('Link'));
+        this.parseLinkHeader(res.headers.get('Link'));
+      })
+    );
+  }
+
+  parseLinkHeader(header) {
+    if (header.length === 0) {
+      return ;
+    }
+
+    const parts = header.split(',');
+    const links = {};
+    parts.forEach( p => {
+      const section = p.split(';');
+      const url = section[0].replace(/<(.*)>/, '$1').trim();
+      const name = section[1].replace(/rel="(.*)"/, '$1').trim();
+      links[name] = url;
+
+    });
+
+    // tslint:disable: no-string-literal
+    this.first  = links['first'];
+    this.last   = links['last'];
+    this.prev   = links['prev'];
+    this.next   = links['next'];
+  }
+
+  public sendGetRequestToUrl(url: string) {
+    return this.httpClient.get(url, { observe: 'response'})
+      .pipe(retry(3),
+        catchError(this.handleError),
+        tap(res => { 
+          console.log(res.headers.get('Link'));
+          this.parseLinkHeader(res.headers.get('Link'));
+        })
       );
   }
 }
